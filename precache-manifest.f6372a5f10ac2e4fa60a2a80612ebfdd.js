83fc8b8b93904f6af8fc509890f0cb38a9eaca25
aajovan <alenjovancom@gmail.com>
2020-09-02 18:47:58 +0200

    Updates

---
+++ precache-manifest.f6372a5f10ac2e4fa60a2a80612ebfdd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2956436d7aaced5798343d24f0ded0d1",
+    "url": "/react_cijene_goriva/index.html"
+  },
+  {
+    "revision": "8de5fb715bd5266ead14",
+    "url": "/react_cijene_goriva/static/css/2.e416123d.chunk.css"
+  },
+  {
+    "revision": "485c909abd067106ed7f",
+    "url": "/react_cijene_goriva/static/css/main.f7f0b784.chunk.css"
+  },
+  {
+    "revision": "8de5fb715bd5266ead14",
+    "url": "/react_cijene_goriva/static/js/2.2cfa4e9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_cijene_goriva/static/js/2.2cfa4e9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "485c909abd067106ed7f",
+    "url": "/react_cijene_goriva/static/js/main.c3fab62f.chunk.js"
+  },
+  {
+    "revision": "a6f43d09b90ad04d194d",
+    "url": "/react_cijene_goriva/static/js/runtime-main.3fdedde1.js"
+  },
+  {
+    "revision": "8b63f34bacb845ab63984740cce5b7e3",
+    "url": "/react_cijene_goriva/static/media/logo.8b63f34b.png"
+  }
+]);
